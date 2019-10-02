73933c7b3da1f2d4086b11e0e22c8ef422cad2b4
Colin Kelleher <colink2009@hotmail.com>
2019-10-02 16:08:28 +0100

    Creating the screen

--- Python_Calculator_GUI.py
+++ Python_Calculator_GUI.py
@@ -1,16 +1,22 @@
 from tkinter import *
 
-# Creating the Frame
-class PythonCalculator:
-    def __init__(self,frame):
+
+class Calculator:
+    def __init__(self, frame):
         self.frame = frame
-        frame.title("Colins Python Calculator")
+        frame.title("Python Calculator")
 
-        self.screen = Text(frame, state='disabled', width=30, height=3,background='yellow', foreground='blue')
+        # create screen widget
+        self.screen = Text(frame, state='disabled', width=30, height=3, background="yellow", foreground="blue")
 
+        # position screen in window
+        self.screen.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
+        self.screen.configure(state='normal')
 
+        # initialize screen value as empty
+        self.equation = ''
 
 
 root = Tk()
-GUI = PythonCalculator(root)
+my_gui = Calculator(root)
 root.mainloop()
